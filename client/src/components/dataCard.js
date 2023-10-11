243ceb1638ccd2fc0c5edb68cc22bcdb161af02b
R3D1NM <r3dinm@gmail.com>
2023-10-11 21:56:21 +0900

    [feat] create new data (OSINT-169)

--- client/src/components/dataCard.js
+++ client/src/components/dataCard.js
@@ -1,35 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from 'react-bootstrap/Card';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
-import { ChevronRight, ChevronLeft, PlusCircle } from 'react-bootstrap-icons';
+import { ChevronRight, ChevronLeft, PlusCircle , PencilSquare, Check} from 'react-bootstrap-icons';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 import Button from 'react-bootstrap/esm/Button';
 import Container from 'react-bootstrap/esm/Container';
+import labels from '../labels';
+import CreateData from './createData';
 
 const DataCard = (props) => {
     const label = props.label
     const nodes = props.nodes
+    const title = labels[label].title
 
     const [selectedEventKey, setSelectedEventKey] = useState('list');
+    const [onEdit, setonEdit] = useState(false)
+
+    // useEffect(() => {
+    //     if(nodes==null){
+    //     setSelectedEventKey('create')
+    //     }
+
+    // }, [nodes])
+    
 
 
-    let title;
-    switch (label) {
-        case "post":
-            title = "Title"
-            break;
-        case "user":
-            title = "Username"
-            break;
-        default:
-            title = ""
-            break;
-    }
-
-
-    const nodeList = nodes.map((node, idx) => (
+    const nodeList = nodes?.map((node, idx) => (
         <Card
         className='mt-1'
         key={node.id}
@@ -55,31 +53,40 @@ const DataCard = (props) => {
         </Card>
     ));
 
-    const selectedNode = nodes.map((node,idx)=>
+    const selectedNode = nodes?.map((node,idx)=>
         selectedEventKey === `selected-${idx}` ? (
             <Card className='mt-1'>
             <Card.Header className='mb-1'>
                 <Button variant="light" size='sm' className='tw-mr-2' onClick={()=>{setSelectedEventKey('list')}}><ChevronLeft/></Button>
                 
                 {node.property[title]}
-                </Card.Header>    
-            {Object.keys(node.property).map(key => (
-            <InputGroup className='mb-1'>
-            <InputGroup.Text id={`${key}-${idx}`}>{key}</InputGroup.Text>
-            <Form.Control
-            placeholder={node.property[key]}
-            />
-            </InputGroup>
+                {onEdit?
+                <Button variant="light" size='sm' className='tw-mr-2' onClick={()=>{setonEdit(false)}}><Check/></Button>
+                :<Button variant="light" size='sm' className='tw-mr-2' onClick={()=>{setonEdit(true)}}><PencilSquare/></Button>}
+            </Card.Header>
+            <Form>
+                {Object.keys(node.property).map(key => (
+                <InputGroup className='mb-1'>
+                <InputGroup.Text id={`${key}-${idx}`}>{key}</InputGroup.Text>
+                <Form.Control
+                placeholder={node.property[key]}
+                disabled={!onEdit}
+                />
+                </InputGroup>
 
-            ))}</Card>): null
+                ))}
+            </Form>
+            </Card>): null
     )
 
     return (
         <Container>
             {selectedEventKey!=="list" ? selectedNode : nodeList}
-            <Card className='align-items-center mt-1 cursor-pointer' onClick={() => { setSelectedEventKey('list') }}>
-                <Button variant="light" size='sm' className='tw-mr-2'><PlusCircle /></Button>
+            {selectedEventKey==="create"?<CreateData label ={label}/>:
+            <Card className='align-items-center mt-1 cursor-pointer' onClick={() => { setSelectedEventKey('create') }}>
+            <Button variant="light" size='sm' className='tw-mr-2'><PlusCircle /></Button>
             </Card>
+            }
 
         </Container>
     
--- client/src/components/dataList.js
+++ client/src/components/dataList.js
@@ -3,18 +3,59 @@ import React, { useEffect, useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import DataCard from './dataCard';
+import lbs from '../labels';
+
+
+///DUMMY DATA///
+const dummy = {
+    "case_id": "1234567890abcdef", //requested case id
+    "data":
+    //neo4j node data list
+    {"post":[
+        {"id":"1234", 
+        "property": {
+        "Url":"blog.naver.com",
+        "Title":"post1",
+        "Created_Date": "2023-09-05",
+        "Type": "naver"
+        }},
+
+        {"id":"5678", 
+        "property": {
+        "Url":"x.com",
+        "Title":"post2",
+        "Created_Date": "2023-09-06",
+        "Type": "twitter"
+        }}
+    ],
+    "user":[
+        {"id":"1111",
+        "property":{
+        "Username": "iche",
+        "Fake": "false"
+        }},
+
+        {"id":"2222",
+        "property":{
+        "Username": "osint",
+        "Fake": "true"
+        }}
+    ]}
+    
+}
+
 
 
 const DataList = (props) => {
     const case_id = props.case_id
     const [caseData, setcaseData] = useState({})
-    const [labels, setlabels] = useState([])
+    const labels = Object.keys(lbs)
     const [isLoad, setisLoad] = useState(false)
-    // const [dataCardList, setdataCardList] = useState([])
+
 
 
     useEffect(() => {
-        // Axios.get(`/case/getData/${case_id}`)
+        // Axios.get(`/data/getData/${case_id}`)
         //     .then((res)=>{
         //     if(res.data){
         //         setcaseData(res.data.data)
@@ -24,59 +65,24 @@ const DataList = (props) => {
         //         setisLoad(false)
         //     }
         //     })
-        
-        ///DUMMY DATA///
-        const dummy = {
-            "case_id": "1234567890abcdef", //requested case id
-            "data":
-            //neo4j node data list
-            {"post":[
-                {"id":"1234", 
-                "property": {
-                "Url":"blog.naver.com",
-                "Title":"post1",
-                "Created_Date": "2023-09-05",
-                "type": "naver"
-                }},
-
-                {"id":"5678", 
-                "property": {
-                "Url":"x.com",
-                "Title":"post2",
-                "Created_Date": "2023-09-06",
-                "type": "twitter"
-                }}
-            ],
-            "user":[
-                {"id":"1111",
-                "property":{
-                "Username": "iche",
-                "Fake": "false"
-                }},
-
-                {"id":"2222",
-                "property":{
-                "Username": "osint",
-                "Fake": "true"
-                }}
-            ]}
-            
-        }
         setcaseData(dummy.data)
-        setlabels(Object.keys(caseData))
-
         
-    }, [caseData, case_id,  labels])
+        
+        
+    }, [caseData, case_id])
 
     
 
     const dataCardList=labels.map((label)=>{
         const labelData = caseData[label]
-        return (
-        <Tab eventKey={label} title={label}>
+        if (labelData){
+        return (<Tab eventKey={label} title={label}>
             <DataCard nodes={labelData} label={label}/>
-        </Tab>
-        )
+        </Tab>)}
+        else{
+        return (<Tab eventKey={label} title={label}>
+            <DataCard nodes={null} label={label}/>
+        </Tab>)}
     })
 
 
