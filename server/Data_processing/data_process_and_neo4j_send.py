20cab89e6eb6142bf0925131af0b963da6fa1476
googyomin <bluesky030@catholic.ac.kr>
2023-10-12 13:56:31 +0900

    [feat] add data module (OSINT-264)

--- server/Data_processing/data_process_and_neo4j_send.py
+++ server/Data_processing/data_process_and_neo4j_send.py
@@ -5,69 +5,78 @@ from py2neo import Graph, Node, Relationship
 graph = Graph("http://localhost:7474", auth=("neo4j", "icheneo4j"),name="neo4j")
 
 # JSON 파일을 읽어오기
-with open("test_data.json", "r", encoding="utf-8") as json_file:
+with open("report_beentack8_simple.json", "r", encoding="utf-8") as json_file:
   data = json.load(json_file)
-  # 중복 데이터 삭제 모듈
-  def remove_duplicates(data_ex):
+  # null값 삭제 모듈
+def remove_null(data):
+    if isinstance(data, dict):
+        return {k: remove_null(v) for k, v in data.items() if v is not None}
+    elif isinstance(data, list):
+        return [remove_null(item) for item in data]
+    else:
+        return data
+# 중복 데이터 삭제 모듈
+def remove_duplicate_dicts(data):
+    unique_data = {}
     seen = set()
-    for key, data_list in data.items():
-        result = []
-        for item in data_list:
-            item_str = str(item)
-            if item_str not in seen:
-                seen.add(item_str)
-                result.append(item)
-        data[key] = result
-# null값 삭제 모듈
-  def remove_all_null(data_ex2):
-    for key, data_list in data.items():
-        for item in data_list:
-            item_copy = item.copy()  # 원본 데이터를 수정하지 않도록 복사본을 만듭니다.
-            for field_name, field_value in item.items():
-                if field_value is None:
-                    del item_copy[field_name]
-            item.clear()
-            item.update(item_copy)
+
+    for key, value in data.items():
+        # 딕셔너리를 정렬된 JSON 문자열로 변환
+        json_str = json.dumps(value, sort_keys=True)
+
+        # 이미 본 JSON 문자열이 아니면 저장하고 seen 집합에 추가
+        if json_str not in seen:
+            seen.add(json_str)
+            unique_data[key] = value
+
+    return unique_data
+
+# 빈 딕셔너리와 None 값을 제거하는 함수 정의
+def remove_empty_dicts_and_none(d):
+    if not isinstance(d, dict):
+        return d
+    return {k: remove_empty_dicts_and_none(v) for k, v in d.items() if v is not None and remove_empty_dicts_and_none(v)}
+
 # 데이터 전처리 (중복값 삭제 + null값 삭제)
-remove_duplicates(data)
-remove_all_null(data)
 
-# json파일 전처리 (GraphDB에 올리기 위해 Dictionary 하나로 데이터를 정의)
-for company in data["Company"]:
-    domain_data = company.pop("Domain", {})
-    company.update({f"Domain-{key}": value for key, value in domain_data.items()})
+cleaned_data = remove_null(data)
+processed_data = remove_duplicate_dicts(cleaned_data)
+data = remove_empty_dicts_and_none(processed_data)
 
-for community in data["Community"]:
-    community_data = community.pop("Post", {})
-    community.update({f"Post-{key}": value for key, value in community_data.items()})
+# JSON 객체의 키 값을 배열에 저장
+keys_array = list(data.keys())
 
-for SNS in data["SNS"]:
-    SNS_data = SNS.pop("Post", {})
-    SNS.update({f"Post-{key}": value for key, value in SNS_data.items()})
+# for 루프를 사용하여 배열 값 출력
+for i in range(len(keys_array)):
+    if i < len(keys_array):
+        if 'site' in data[keys_array[i]]:
+            site_data = data[keys_array[i]]['site']
+            data[keys_array[i]].update({f'site-{key}': value for key, value in site_data.items()})
+            del data[keys_array[i]]['site']
+        if 'status' in data[keys_array[i]]:
+            status_data = data[keys_array[i]]['status']
+            data[keys_array[i]].update({f'status-{key}': value for key, value in status_data.items()})
+            del data[keys_array[i]]['status']
+        if 'ids_usernames' in data[keys_array[i]]:
+            ids_usernames_data = data[keys_array[i]]['ids_usernames']
+            data[keys_array[i]].update({f'ids_usernames-{key}': value for key, value in ids_usernames_data.items()})
+            del data[keys_array[i]]['ids_usernames']
+        if 'status-ids' in data[keys_array[i]]:
+            ids_usernames_data = data[keys_array[i]]['status-ids']
+            data[keys_array[i]].update({f'status-ids-{key}': value for key, value in ids_usernames_data.items()})
+            del data[keys_array[i]]['status-ids']
+    else:
+        break
 
-new_blog_data = []
-for blog in data["Blog"]:
-    posts = blog.get("Post", [])
-    for post in posts:
-        new_blog = {
-            "Blog-Name": blog.get("Name", ""),
-            "Blog-URL": blog.get("URL", ""),
-            "Blog-Type": blog.get("type", ""),
-        }
-        new_blog.update({f"Post-{key}": value for key, value in post.items()})
-        new_blog_data.append(new_blog)
+# 각 항목을 []로 묶는 작업 수행
+for key, value in data.items():
+    data[key] = [value]
 
-data["Blog"] = new_blog_data
-
-for blog in data["Blog"]:
-    blog_data = blog.pop("Post-Comment", {})
-    blog.update({f"Post-Comment-{key}": value for key, value in blog_data.items()})
-
-# 처리한 데이터 그래프에 저장. (없는 데이터 항목 있어도 나머지 데이터 추가 가능)
+# # Neo4j 노드 생성
 for category, items in data.items():
-    for item in items:
-        node = Node(category, **{k: v for k, v in item.items() if v is not None})
-        graph.create(node)
+ for item in items:
+    node = Node(category, **{k: v for k, v in item.items() if v is not None})
+ graph.create(node)
 # 관계 생성 예시
 # Alice와 Bob 간의 "KNOWS" 관계 생성
 # alice = graph.nodes.match("Person", name="Alice").first()
