73e0a384a07d8449a34769fc587b23d13a69e4e4
shj <aa753@knu.ac.kr>
2023-10-13 11:20:57 +0900

    create data.py

---
+++ server/case/data.py
@@ -0,0 +1,16 @@
+import datetime
+
+# flaks 
+from flask import request, jsonify,Blueprint
+
+from db_conn.mongo.init import db 
+from db_conn.mongo.models import CaseModel
+
+
+bp = Blueprint('data', __name__, url_prefix='/getData')
+
+# @bp.route('/<case_id>',methods=['GET'])
+# def get_data(case_id):
+#     res = {}
+#     res['case_id'] = case_id,
+#     res['data'] +
