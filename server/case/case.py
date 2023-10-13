d57bc240b6e8a54d33e2bea804ada57998c2f53f
shj <aa753@knu.ac.kr>
2023-10-13 10:45:54 +0900

    test com

--- server/case/case.py
+++ server/case/case.py
@@ -27,6 +27,9 @@ def check_json_not_null(input):
                     return False
     return True
 
+@bp.route('/')
+def test():
+	return 'Hello World!'
 
 @bp.route('/createCase',methods=['POST'])
 def create_case():
@@ -49,6 +52,97 @@ def create_case():
     return jsonify({'Message':'Success'}),200
 
 
+@bp.route('/getCaseList')
+def getcaselist():
+    try:
+        all_cases = CaseModel.objects().all()
+
+        cases_list = []
+
+        for case in all_cases:
+            case_data = {
+                "case_id": case.case_id,
+                "case_name": case.case_name,
+                "case_num": case.case_num,
+                "investigator": case.investigator,
+                "description": case.description,
+                "created_date": case.created_date
+            }
+            cases_list.append(case_data)
+
+        return jsonify(cases_list), 200
+		#return cases_list, 200
+    except PyMongoError as e:
+        print(f'Error while retrieving all cases: {e}')
+        return jsonify({'Message': 'Error while retrieving all cases'}), 500
+    
+
+@bp.route('/getCaseInfo/<int:case_id>')
+def get_case(case_id):
+    try:
+        case = CaseModel.objects.get(case_id=case_id)
+        
+        if case:
+            response_data = {
+                "case_id": case.case_id,
+                "case_name": case.case_name,
+                "case_num": case.case_num,
+                "investigator": case.investigator,
+                "description": case.description,
+                "created_date": case.created_date
+            }
+            
+            return jsonify(response_data), 200
+        else:
+            return jsonify({'Message': f'Case with ID {case_id} not found'}), 404
+    except PyMongoError as e:
+        print(f'Error while retrieving case: {e}')
+        return jsonify({'Message': 'Error while retrieving case'}), 500
+
+@bp.route('/deleteCase/<int:case_id>')
+def delete_case(case_id):
+    try:
+        result = CaseModel.objects(case_id=case_id).delete()
+
+        if result['n'] > 0:
+            return jsonify({'Message': f'Case with ID {case_id} has been deleted'}), 200
+        else:
+            return jsonify({'Message': f'Case with ID {case_id} not found'}), 404
+    except PyMongoError as e:
+        print(f'Error while deleting case: {e}')
+        return jsonify({'Message': 'Error while deleting case'}), 500
 
 
+@bp.route('/editCase', methods=['POST'])
+def edit_case():
+    request_data = request.get_json()
 
+    if "case_name" in request_data and "case_number" in request_data and "investigator" in request_data and "description" in request_data:
+        caseName = request_data["case_name"]
+        caseNum = request_data["case_number"]
+        investigator = request_data["investigator"]
+        description = request_data["description"]
+
+        try:
+            case_id = request_data.get("case_id")
+            if case_id:
+                updated_data = {
+                    "case_name": caseName,
+                    "case_num": caseNum,
+                    "investigator": investigator,
+                    "description": description
+                }
+
+                result = CaseModel.objects(case_id=case_id).update(**updated_data)
+
+                if result['n'] > 0:
+                    return "Your modification request was processed successfully.", 200
+                else:
+                    return "Case not found for the given case_id.", 404
+            else:
+                return "Case ID is missing in the request data.", 400
+        except PyMongoError as e:
+            print(f'Error while updating case: {e}')
+            return jsonify({'Message': 'Error while updating case'}), 500
+    else:
+        return "Required field is missing in the request data.", 400
