9fa5671fdd08396620fc04e39133207e84f5288c
D7MeKz <65109465+D7MeKz@users.noreply.github.com>
2023-10-19 15:29:34 +0900

    [feat] Add Extension Persistence and fix posting relationship from extension(OSINT-267)

--- server/src/resource/ext.py
+++ server/src/resource/ext.py
@@ -9,6 +9,14 @@ from db_conn.neo4j.lib.relation_manager import RelationManager
 
 bp = Blueprint('extension', __name__, url_prefix='/graph/ext')
 
+def compare_post_user_username(post_obj:Post, user_obj:SurfaceUser):
+    if hasattr(post_obj, 'writer') and hasattr(user_obj,'username'):
+        if post_obj.writer == user_obj.username:
+            if not user_obj.posting.is_connected(post_obj):
+                user_obj.posting.connect(post_obj)
+            return True
+    return False 
+
 @bp.route('/create',methods=["POST"])
 def create_node():
     req = request.get_json()
@@ -26,18 +34,21 @@ def create_node():
     if req_label == 'SurfaceUser':
         node_id = SurfaceUser.node_exists_url(req['url'])
         if node_id is not None:
-            node = SurfaceUser.update_node_properties(node_id, **req_arg)
-            if node is False:
+            user_obj = SurfaceUser.update_node_properties(node_id, **req_arg)
+            if user_obj is False:
                 return jsonify({'Error':'Node update Error '}), 500
-            if hasattr(node, 'username') and node.username is not None:
-                RelationManager.post_contain_user(node.username)
+
+            post_obj = Post.nodes.first_or_none(writer=node.username)
+            if post_obj:
+                compare_post_user_username(post_obj=post_obj,user_obj=user_obj) 
         else:
             req_arg['url'] = req['url']
-            node = SurfaceUser.create_node(req_arg)
-            if hasattr(node, 'username') and node.username is not None:
-                RelationManager.post_contain_user(node.username)
-            if not node:
+            user_obj = SurfaceUser.create_node(req_arg)
+            if not user_obj:
                 return jsonify({'Error':'Node creation Error'}), 500
+            post_obj = Post.nodes.first_or_none(writer=user_obj.username)
+            if post_obj:
+                compare_post_user_username(post_obj=post_obj,user_obj=user_obj) 
             
     elif req_label == 'Domain':
         node_id = Domain.node_exists_url(req['url'])
@@ -53,18 +64,20 @@ def create_node():
     elif req_label == 'Post':
         node_id = Post.node_exists_url(req['url'])
         if node_id:
-            post_node = Post.update_node_properties(node_id, **req_arg)
-            if post_node is False:
+            post_obj = Post.update_node_properties(node_id, **req_arg)
+            if post_obj is False:
                 return jsonify({'Error':'Node update Error '}), 500
-            if hasattr(post_node, 'writer') and post_node.writer is not None:
-                RelationManager.post_contain_user(post_node.writer)
+            user_obj = SurfaceUser.nodes.first_or_none(username=post_obj.writer)
+            if user_obj:
+                compare_post_user_username(post_obj,user_obj)
         else:
             req_arg['url'] = req['url']
-            post_node = Post.create_node(req_arg)
-            if not post_node:
+            post_obj = Post.create_node(req_arg)
+            if not post_obj:
                 return jsonify({'Error':'Node creation Error'}), 500
-            if hasattr(post_node, 'writer') and post_node.writer is not None:
-                RelationManager.post_contain_user(post_node.writer)
+            user_obj = SurfaceUser.nodes.first_or_none(username=post_obj.writer)
+            if user_obj:
+                compare_post_user_username(post_obj,user_obj)
 
     return jsonify({'Message':'Success'}), 200
     
