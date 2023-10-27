e9436041ece25577ca82eb308c01dbfd4fedb2db
D7MeKz <65109465+D7MeKz@users.noreply.github.com>
2023-10-27 12:42:34 +0900

    [fix] Fix model package(OSINT-276)

---
+++ server/db_conn/neo4j/models/__init__.py
@@ -0,0 +1,6 @@
+from .models.user import Person, SurfaceUser,DarkUser, Company
+from .models.domain import Domain
+from .models.post import Post, Comment
+from .models.user_info import Email,Wallet, Phone, Message
+
+__all__ = ['Person', 'SurfaceUser', 'DarkUser', 'Company', 'Domain', 'Post', 'Comment', 'Email', 'Wallet', 'Phone', 'Message']
--- server/src/resource/graph.py
+++ server/src/resource/graph.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify,Blueprint
 
-from db_conn.neo4j import db
+from server.db_conn.neo4j import db
 from db_conn.neo4j.models.user import SurfaceUser
 from db_conn.neo4j.models.post import Post
 
