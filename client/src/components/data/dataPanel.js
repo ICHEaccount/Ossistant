1b08e4dd254e28e9090b8e18f15cdaebae0a36a7
R3D1NM <r3dinm@gmail.com>
2023-10-31 16:14:18 +0900

    [feat] dropdown version datapanel

--- client/src/components/data/dataPanel.js
+++ client/src/components/data/dataPanel.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
-import { Alert, Button, Nav, Tab } from 'react-bootstrap';
+import { Alert, Button, Nav, NavDropdown, Tab, Tabs } from 'react-bootstrap';
 import DataList from './dataList';
 import { Database , Gear, FileEarmarkText, ArrowRepeat} from 'react-bootstrap-icons';
 import ToolList from '../tool/toolList';
 import Report from '../report';
 import Axios  from 'axios';
 import { useSelector, useDispatch } from 'react-redux'
-import {panelChange} from '../../reducers/node'
+import {panelChange,labelChange,categoryChange} from '../../reducers/node'
 import RunList from '../run/runList';
+import labels,{category} from '../../labels';
+import DataCard from './dataCard';
 
 const DataPanel = (props) => {
     const panel = useSelector(state => state.node.panel)
+    const selcted_label = useSelector(state => state.node.label)
     const dispatch = useDispatch()
     const case_id = props.case_id
     const caseData = props.caseData
@@ -20,15 +23,37 @@ const DataPanel = (props) => {
     const toolState = props.toolState
     const newData=props.newData
     
+    const categoryList = Object.keys(category).map((tag)=>{
+        const list = category[tag]
+        const dataCardList = list.map((label)=>{
+            return <Tab eventKey={label} title={label} >
+                <DataCard nodes={caseData[label]!==undefined?caseData[label]:null} label={label} newData={newData[label]?newData[label]:null}/>
+                </Tab>
+        })
+        return <Tab.Pane eventKey={`data-${tag}`} title={tag}>
+            <Tabs activeKey={selcted_label} onSelect={(k)=>dispatch(labelChange(k))} justify>
+                    {dataCardList}
+            </Tabs>
+        </Tab.Pane>
+    })
+
     const LeftTabs = () => {
         return (
         <Tab.Container id="left-tabs-example" activeKey={panel} onSelect={(k)=>{dispatch(panelChange(k))}}>
-            <Nav variant="pills" fill justify className=" p-1 tw-border tw-rounded-md mb-1">
-                <Nav.Item >
+            <Row>
+            <Col sm={3} className=''>
+            <Nav variant="pills" fill justify className=" flex-column p-1 tw-border tw-rounded-md mb-1">
+                <NavDropdown drop='end' title="Data" onSelect={(k)=>{dispatch(categoryChange(k.split("-")[1]))}}>
+                    <NavDropdown.Item eventKey="data-Subject">Subject</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="data-Site">Site</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="data-Info">Info</NavDropdown.Item>
+                    
+                </NavDropdown> 
+                {/* <Nav.Item >
                 <Nav.Link eventKey="data-list" className='tw-w-full tw-h-full d-flex justify-content-center align-items-center'>
                         <Database size="30px" />
                     </Nav.Link>
-                </Nav.Item>
+                </Nav.Item> */}
                 <Nav.Item>
                     <Nav.Link eventKey="tool-list" className='tw-w-full tw-h-full d-flex justify-content-center align-items-center'>
                         <Gear size="30px"/>
@@ -45,12 +70,17 @@ const DataPanel = (props) => {
                     </Nav.Link>
                 </Nav.Item>
             </Nav>
+            </Col>
+            <Col sm={9}>
             <Tab.Content>
-            <Tab.Pane eventKey="data-list"><DataList case_id={case_id} caseData={caseData} newData={newData}/></Tab.Pane>
+            {/* <Tab.Pane eventKey="data-list"><DataList case_id={case_id} caseData={caseData} newData={newData}/></Tab.Pane> */}
+            {categoryList}
             <Tab.Pane eventKey="tool-list"><ToolList case_id={case_id} caseData={caseData} toolState={toolState} toolrunner={toolrunner}/></Tab.Pane>
             <Tab.Pane eventKey="run-list"><RunList case_id={case_id} toolState={toolState} toolrunner={toolrunner}/></Tab.Pane>
             <Tab.Pane eventKey="report"><Report case_id={case_id} caseData={caseData}/></Tab.Pane>
             </Tab.Content>
+            </Col>
+            </Row>
         </Tab.Container>
         );
     }
