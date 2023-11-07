2d1c4bdfe3e74eca3e1d5552a486c4526af72c67
R3D1NM <r3dinm@gmail.com>
2023-11-07 14:20:45 +0900

    [feat] add footer

---
+++ client/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+    <footer className='tw-bg-gradient-to-b tw-fixed tw-left-0 tw-bottom-0 tw-w-full tw-p-1 tw-bg-bright-peach  tw-text-center tw-text-peach '>
+        <span>OSSISTANT <small>v.0.2</small> by ICHE </span>
+        
+    </footer>
+    )
+}
+
+export default Footer
