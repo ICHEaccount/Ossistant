8bf05dbe98870980615c3c764afb1e36469f2e70
R3D1NM <r3dinm@gmail.com>
2023-11-24 10:48:39 +0900

    [chore] edit test period

--- client/src/pages/test.js
+++ client/src/pages/test.js
@@ -25,11 +25,11 @@ const Test = () => {
                     </Form>
                     {korean?(<Card.Text>
                         <p>테스트 대상: 전/현직 사이버 수사관 </p>
-                        <p>테스트 기간: 2023-11-22 ~ 2023-11-25</p>
+                        <p>테스트 기간: 2023-11-22 ~ 2023-11-27</p>
                     </Card.Text>)
                     :(<Card.Text>
                         <p>Tester: Former and Current Cybercrime Investigators </p>
-                        <p>Period: 22-11-2023 ~ 25-11-2023</p>
+                        <p>Period: 22-11-2023 ~ 27-11-2023</p>
                     </Card.Text>)}
                     </Card.Body>
                     <Card.Body>
