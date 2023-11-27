6092c30093ddd2898320cb1517cca7a49e0e4f7d
D7MeKz <65109465+D7MeKz@users.noreply.github.com>
2023-11-27 18:43:45 +0900

    [feat] Node, Relationship validation(OSINT-333)

--- server/osint/tools/config/tool_result_config.py
+++ server/osint/tools/config/tool_result_config.py
@@ -12,9 +12,17 @@ UPDATE_PROPERTY = 1
 TOOL_RESULT_MATCH = {
     "03" : {
         'type': UPDATE_PROPERTY,
-        'input_label': SurfaceUser,
-        'result_label' : SurfaceUser,
+        'input_label': None,
+        'result_label' : None,
         'property': 'registered',
-        'db_name' : 'site'
+        'db_property_name' : None
+    },
+    # Sample for create_node mode 
+    "04": {
+        'type':CREATE_NODE,
+        'input_label':SurfaceUser,
+        'result_label' : Wallet,
+        'property': 'wallet',
+        'db_property_name': None
     }
 }
