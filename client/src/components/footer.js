5ddcb304659696bbcd8ce6fd24c908b467f956d8
R3D1NM <r3dinm@gmail.com>
2023-11-27 23:02:31 +0900

    [fix] set relation size to 49vh (OSINT-335)

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-    <footer className='tw-border-t-peach tw-fixed tw-left-0 tw-bottom-0 tw-w-full tw-p-1 tw-bg-bright-peach  tw-text-center  tw-text-dark-navy'>
+    <footer className='tw-border-t-peach tw-fixed tw-left-0 tw-bottom-0 tw-w-full tw-p-0.5 tw-bg-bright-peach  tw-text-center  tw-text-dark-navy'>
         <span>OSSISTANT <small>v.0.3</small> by ICHE </span>
         
     </footer>
