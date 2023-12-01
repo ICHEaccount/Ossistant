9a4c2740ed4214d6d6b9e091e8dd452c4fe9ca6c
R3D1NM <r3dinm@gmail.com>
2023-12-01 19:16:12 +0900

    [feat] new labels.js to create/view data

---
+++ client/src/labels_old.js
@@ -0,0 +1,77 @@
+const labels = {
+    "Post":{
+        "title":"title",
+        "category":"Site",
+        "properties":["title","writer","url","content","created_date","note"],
+        "list":[]
+    },
+    "Comment":{
+        "title":"content",
+        "category":"Site",
+        "properties":["name","content","created_date","url"],
+        "list":[]
+    },
+    "SurfaceUser":{
+        "title":"username",
+        "category":"Subject",
+        "properties":["username","imposter","registered","note"],
+        "list":["registered"]
+
+    },
+    "DarkUser":{
+        "title":"username",
+        "category":"Subject",
+        "properties":["username","rank","regdate","post_num","comment_num","imposter","registered","note"],
+        "list":["registered"]
+    },
+    "Person":{
+        "title":"name",
+        "category":"Subject",
+        "properties":["name","imposter","note"],
+        "list":[]
+    },
+    "Company":{
+        "title":"name",
+        "category":"Subject",
+        "properties":["name","imposter","business_num","location","note"],
+        "list":[]
+    },
+    "Domain":{
+        "title":"domain",
+        "category":"Site",
+        "properties":["domain","regdate","status","leaked","note"],
+        "list":[]
+    },
+    "Phone":{
+        "title":"number",
+        "category":"Info",
+        "properties":["number","note"],
+        "list":[]
+    },
+    "Message":{
+        "title":"sender",
+        "category":"Info",
+        "properties":["sender","content","date","note"],
+        "list":[]
+    },
+    "Email":{
+        "title":"email",
+        "category":"Info",
+        "properties":["email","leaked","note"],
+        "list":[]
+    },
+    "Wallet":{
+        "title":"wallet",
+        "category":"Info",
+        "properties":["wallet","wallet_type","note"],
+        "list":[]
+    }
+}
+
+export const category = {
+    "Subject":["SurfaceUser","DarkUser","Company","Person"],
+    "Site":["Domain","Post","Comment"],
+    "Info":["Email","Phone","Message","Wallet"],
+}
+
+export default labels
