656924f51e4e935ab3414f76bb031ce5e9d29779
bobfox <k.fox0229@gmail.com>
2023-12-12 15:35:38 +0900

    [fix] whois exception (not complete) (OSINT-322)

--- server/osint/tools/lib/tool_whois.py
+++ server/osint/tools/lib/tool_whois.py
@@ -120,7 +120,10 @@ def report_whois(case_id, run):
     node_created = []
 
     for email in email_list:  # Deleted 'filtered_email_list'
-        match = re.match(pattern, email)
+        try:
+            match = re.match(pattern, email)  # TODO: TypeError: expected string or byte-like object
+        except TypeError as e:
+            return f'Debug: email is {email}'  # email is none
         if match:  # SHOULD match.
             username = match.group(1)
             domain = match.group(2)  # It's "gmail", not "gmail.com".
